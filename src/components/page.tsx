f904bee84122d36bd7353c0dfc4710ed9948c9e2
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-04-25 08:25:41 +0000

    jui

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -60,10 +60,10 @@ export default async function ProductDetailPage({ params }: PageProps) {
 
       {/* Body Section - 50/50 Split */}
       <section className="max-w-7xl mx-auto px-6 py-16 md:py-24">
-        <div className="flex flex-col lg:flex-row gap-12 lg:gap-16 items-start">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 md:gap-16 items-start">
           
           {/* First 50%: Image */}
-          <div className="w-full lg:w-1/2">
+          <div className="w-full">
             <div className="relative h-[400px] md:h-[500px] lg:h-[600px] w-full rounded-2xl overflow-hidden shadow-xl border border-black/10 bg-white">
               {coverImage ? (
                 <Image 
@@ -83,7 +83,7 @@ export default async function ProductDetailPage({ params }: PageProps) {
           </div>
 
           {/* Next 50%: Content */}
-          <div className="w-full lg:w-1/2 flex flex-col">
+          <div className="w-full flex flex-col overflow-hidden">
             <h2 className="font-heading text-3xl md:text-4xl text-black font-bold mb-6">
               Product Overview
             </h2>
