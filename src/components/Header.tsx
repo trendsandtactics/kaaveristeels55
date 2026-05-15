f76ad2abcb21f50c71547122a7bff0767eeb6efe
trendsandtactics <trendsntactics@gmail.com>
2026-05-15 18:24:26 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -123,7 +123,7 @@ export default function Header() {
                 width={190}
                 height={60}
                 priority
-                className="h-12 md:h-12 xl:h-11 w-auto object-contain"
+                className="h-14 md:h-14 xl:h-11 w-auto object-contain"
               />
             </Link>
 
