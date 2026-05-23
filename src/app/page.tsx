8ee21231a52f782243939851affbb5e3cdc35e23
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-05-23 10:16:31 +0000

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,39 +10,39 @@ import TrustOnsite from "@/components/trustonsite";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center w-full relative gap-12 md:gap-16">
+    <div className="flex flex-col items-center w-full relative">
       
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Scrollytelling Hero Area */}
         <SteelScroll />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* About Section */}
         <HomeAbout />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Products Section */}
         <HomeProducts />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Green Steel Certification Section */}
         <GreenSteel />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Calculator Section */}
         <SteelCalculator />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Trust Section */}
         <TrustOnsite />
       </section>
 
-      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col py-8 md:py-12">
+      <section className="scroll-section scroll-mt-20 md:scroll-mt-24 w-full flex flex-col">
         {/* Map Section */}
         <MapEmbed />
       </section>
