02a86194f900da6151b146d3f1ab35d53d9cadb3
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-06-06 12:32:30 +0000

    ik

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,11 @@ export default function Home() {
       // Enable native CSS scroll snapping for desktop and tablet landscape (>= 1024px)
       if (window.innerWidth >= 1024) {
         document.documentElement.style.scrollSnapType = "y mandatory";
+        // Fix known browser bug where scroll-smooth + scroll-snap skips to bottom
+        document.documentElement.style.scrollBehavior = "auto";
       } else {
         document.documentElement.style.scrollSnapType = "";
+        document.documentElement.style.scrollBehavior = "";
       }
 
       const sections = Array.from(document.querySelectorAll(".scroll-section")) as HTMLElement[];
@@ -59,43 +62,44 @@ export default function Home() {
     return () => {
       window.removeEventListener("resize", handleResize);
       document.documentElement.style.scrollSnapType = "";
+      document.documentElement.style.scrollBehavior = "";
     };
   }, []);
 
   return (
     <div className="flex flex-col items-center w-full relative pt-20 lg:pt-24 overflow-x-hidden">
       
-      <section className="w-full flex flex-col snap-start">
+      <section className="w-full flex flex-col snap-start snap-always">
         {/* Scrollytelling Hero Area */}
         <SteelScroll />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* About Section */}
         <HomeAbout />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* Products Section */}
         <HomeProducts />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* Green Steel Certification Section */}
         <GreenSteel />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* Calculator Section */}
         <SteelCalculator />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* Trust Section */}
         <TrustOnsite />
       </section>
 
-      <section className="scroll-section snap-start scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
+      <section className="scroll-section snap-start snap-always scroll-mt-20 lg:scroll-mt-24 w-full flex flex-col lg:justify-center min-h-[calc(100svh-5rem)] lg:min-h-[calc(100svh-6rem)] py-12 lg:py-0">
         {/* Map Section */}
         <MapEmbed />
       </section>
