0a6ec41207ceba96d716a6cac93ca2fd38722768
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-06-24 16:36:22 +0530

    Update page.tsx

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,4 +1,6 @@
 import AboutHero from "@/components/AboutHero";
+import AboutContent from "@/components/AboutContent";
+
 
 export const metadata = {
     title: "About Us | KAAVERI TMT & STRUCTURAL",
@@ -9,6 +11,7 @@ export default function AboutUsPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between w-full relative bg-background">
             <AboutHero />
+            <AboutContent />
         </main>
     );
 }
