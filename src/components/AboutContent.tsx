4b318806a25fbb010d8a6203104f19d5f175c36a
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-07-03 08:22:20 +0000

    scroll

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -111,66 +111,37 @@ export default function FullWidthQualitySection() {
       </div>
 
       {/* Trust Strip */}
-      <div className="w-full px-4 sm:px-8 lg:px-10 xl:px-12 2xl:px-24 mt-8 lg:mt-10 2xl:mt-12">
-        <div className="overflow-hidden rounded-2xl border border-gray-100 shadow-lg bg-white">
-          <div className="flex flex-col lg:flex-row">
-            
-            {/* Left Box - Controlled width for laptops */}
-            <div className="lg:w-40 xl:w-48 2xl:w-72 bg-gradient-to-r from-red-700 to-red-600 text-white flex items-center justify-center p-4 lg:p-4 xl:p-6 2xl:p-8 shrink-0">
-              <div className="flex items-center gap-2 xl:gap-3">
-                <ShieldCheck className="w-5 h-5 xl:w-6 xl:h-6 2xl:w-7 2xl:h-7" />
-                <span className="uppercase tracking-wider font-bold text-sm xl:text-base 2xl:text-lg">
-                  Trusted By
-                </span>
-              </div>
+      <div className="w-full mt-12 lg:mt-16 2xl:mt-20">
+        <div className="bg-white py-8 lg:py-12 border-y border-gray-100">
+          <div className="text-center mb-8">
+            <div className="flex items-center justify-center gap-3">
+              <ShieldCheck className="w-6 h-6 text-red-600" />
+              <h3 className="font-bold text-lg text-gray-800 uppercase tracking-wider">
+                Trusted By
+              </h3>
             </div>
-
-            {/* Middle - Logos Grid */}
-            <div className="flex-1 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-              {approvals.map((item) => (
-                <div
-                  key={item.name}
-                  className="group lg:border-l border-t lg:border-t-0 border-gray-100 p-4 lg:p-3 xl:p-4 2xl:p-8 flex flex-col items-center justify-center hover:bg-gray-50 transition"
+          </div>
+          <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+            <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+              {[...approvals, ...approvals].map((item, index) => (
+                <li
+                  key={`${item.name}-${index}`}
+                  className="flex flex-col items-center justify-center w-40"
                 >
-                  <div className="relative w-16 h-12 xl:w-20 xl:h-14 2xl:w-20 2xl:h-16 mb-2 xl:mb-3 group-hover:scale-110 transition-transform flex items-center justify-center">
+                  <div className="relative h-16 w-full">
                     <Image
                       src={item.logo}
-                      alt={`${item.name} Logo`}
+                      alt={item.name}
                       fill
                       className="object-contain"
-                      sizes="(max-width: 1280px) 64px, 80px"
                     />
                   </div>
-
-                  <span className="text-[10px] xl:text-xs font-semibold text-gray-800 tracking-wide text-center">
+                  <span className="mt-2 text-xs text-center font-semibold text-gray-600">
                     {item.name}
                   </span>
-                </div>
+                </li>
               ))}
-            </div>
-
-            {/* Right Box - Controlled width for laptops */}
-            <div className="lg:w-48 xl:w-56 2xl:w-80 lg:border-l border-t lg:border-t-0 border-gray-100 bg-gradient-to-br from-gray-50 to-white flex items-center justify-center p-6 lg:p-4 xl:p-6 2xl:p-10 shrink-0">
-              <div className="text-center w-full">
-                <div className="w-10 h-10 xl:w-12 xl:h-12 2xl:w-14 2xl:h-14 rounded-full bg-red-100 flex items-center justify-center mx-auto mb-3 xl:mb-4">
-                  <ShieldCheck className="text-red-600 w-5 h-5 xl:w-6 xl:h-6 2xl:w-7 2xl:h-7" />
-                </div>
-
-                <h4 className="font-bold text-sm xl:text-base 2xl:text-2xl text-gray-900 leading-snug">
-                  Trusted for
-                  <br />
-                  Government,
-                  <br />
-                  Infrastructure,
-                  <br className="hidden lg:block" />
-                  <span className="lg:hidden"> Commercial & </span>
-                  <span className="hidden lg:inline">Commercial &</span>
-                  <br />
-                  Residential Projects
-                </h4>
-              </div>
-            </div>
-
+            </ul>
           </div>
         </div>
       </div>
