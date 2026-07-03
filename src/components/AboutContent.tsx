53b9c5871405c08155254d34d4577ec43f145370
Karthikentrepreneur <karthikjungleemara@gmail.com>
2026-07-03 10:20:35 +0000

    commi

--- src/components/AboutContent.tsx
+++ src/components/AboutContent.tsx
@@ -134,13 +134,13 @@ export default function FullWidthQualitySection() {
                     className="flex-shrink-0 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/5 px-4 py-2 flex flex-col items-center justify-center"
                   >
                     {/* Logo */}
-                    <div className="flex h-16 w-full items-center justify-center">
+                    <div className="flex h-24 w-full items-center justify-center">
                       <Image
                         src={item.logo}
                         alt={`${item.name} Logo`}
-                        width={90}
-                        height={60}
-                        className="block max-h-14 w-auto object-contain"
+                        width={130}
+                        height={90}
+                        className="block max-h-20 xl:max-h-24 w-auto object-contain"
                       />
                     </div>
                     {/* Name */}
